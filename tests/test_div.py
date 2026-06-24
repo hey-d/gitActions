a2e1f5ab4c5f7e58676f2fbfed85d98c94983dea
Dushyant Manghani <dushyantmanghani@gmail.com>
2026-06-24 15:12:36 +0530

    4 tests added modified

--- tests/test_div.py
+++ tests/test_div.py
@@ -4,7 +4,7 @@ def test_div_positive():
     assert div(6,2)==3
     
 def test_div_negative():
-    assert sum(6, -2)==-3
+    assert div(6, -2)==-3
     
 def div_num_small():
-    assert sum(2, 6)==0
+    assert div(2, 6)==0
